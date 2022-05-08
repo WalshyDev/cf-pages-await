272cfc2867e151b5469a0da52652a6fc5f94c319
Daniel Walsh <walshy@cloudflare.com>
2022-05-08 10:48:39 +0100

    Support API tokens

--- dist/action.js
+++ dist/action.js
@@ -11273,17 +11273,22 @@ var waiting = true;
 var ghDeployment;
 var markedAsInProgress = false;
 async function run() {
-  const accountEmail = core.getInput("accountEmail", { required: true, trimWhitespace: true });
-  const apiKey = core.getInput("apiKey", { required: true, trimWhitespace: true });
+  const accountEmail = core.getInput("accountEmail", { required: false, trimWhitespace: true });
+  const apiKey = core.getInput("apiKey", { required: false, trimWhitespace: true });
+  const apiToken = core.getInput("apiToken", { required: false, trimWhitespace: true });
   const accountId = core.getInput("accountId", { required: true, trimWhitespace: true });
   const project = core.getInput("project", { required: true, trimWhitespace: true });
   const token = core.getInput("githubToken", { required: false, trimWhitespace: true });
   const commitHash = core.getInput("commitHash", { required: false, trimWhitespace: true });
+  if (!validateAuthInputs(apiToken, accountEmail, apiKey)) {
+    return;
+  }
+  const authHeaders = apiToken !== "" ? { Authorization: `Bearer ${apiToken}` } : { "X-Auth-Email": accountEmail, "X-Auth-Key": apiKey };
   console.log("Waiting for Pages to finish building...");
   let lastStage = "";
   while (waiting) {
     await sleep();
-    const deployment = await pollApi(accountEmail, apiKey, accountId, project, commitHash);
+    const deployment = await pollApi(authHeaders, accountId, project, commitHash);
     if (!deployment) {
       console.log("Waiting for the deployment to start...");
       continue;
@@ -11317,16 +11322,23 @@ async function run() {
     }
   }
 }
-async function pollApi(accountEmail, apiKey, accountId, project, commitHash) {
+function validateAuthInputs(token, email, key) {
+  if (token !== "") {
+    return true;
+  }
+  if (email !== "" && key !== "") {
+    return true;
+  }
+  core.setFailed("Please specify authentication details! Set either `apiToken` or `accountEmail` + `accountKey`!");
+  return false;
+}
+async function pollApi(authHeaders, accountId, project, commitHash) {
   var _a2, _b, _c;
   let res;
   let body;
   try {
     res = await fetch(`https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/${project}/deployments?sort_by=created_on&sort_order=desc`, {
-      headers: {
-        "X-Auth-Email": accountEmail,
-        "X-Auth-Key": apiKey
-      }
+      headers: { ...authHeaders }
     });
   } catch (e) {
     core.error(`Failed to send request to CF API - network issue? ${e.message}`);
